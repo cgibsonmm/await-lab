6109829fd6a0f5dab7ce24d32085f8e139e3b581
Steve VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-04-24 17:42:34 -0400

    Update master branch

--- async.js
+++ async.js
@@ -1,50 +1,39 @@
-// Async Await Lab
-// (API Docs)[https://recipix.app/api/docs]
+// Async/Await w/ Try/Catch
+// API docs https://restcountries.eu/#api-endpoints-full-name
 
-// PART 1: Read Recipe Name
-findRecipes('banana');
-async function findRecipes(recipeName) {
-  // read recipe
-  let response = await axios.get(
-    `https://recipix.app/api/search/?q=${recipeName}`
-  );
+// Step 1: Fetch Country data
 
-  // document.getElementById('recipe-title').innerHTML = ???
-  // document.getElementById('recipe-url').href = ???
-
-  // showIngredients( ??? );
+function fetchData(country) {
+// Get country data
+  const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+  // Write the rest of the code here.
 }
 
-// PART 2: Read Recipe Ingredients
-function showIngredients(recipe) {
-  // show the ingredients
-  // iterare over the response data
-  // insert each ingredient into the DOM.
-  // choose whichever type of iterator for each ingredient..
-  let ingredients = recipe.ingredients;
-  document.getElementById('recipe-ingredients').innerHTML = '';
-  ingredients.forEach(function(ingredient) {
-    let listItem = document.createElement('li');
-    // listItem.innerHTML = ???
-    // listItem.addEventListener('???', ???);
-    document.getElementById('recipe-ingredients').append(listItem);
-  });
-  // return ingredients;
+// Step 2: Rewrite using Try/Catch & Async/Await
+
+function fetchData(country) {
+  const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+  // Write the rest of the code here.
+}
+// Step 3: Create dynamic HTML
+
+function showCountryData(data) {
+  // ???
 }
 
-// PART 3: Display Recipe from Ingredients
+// Step 4: Append Country Data
 
-// For each ingredient displayed, add a `click` event listener that
-// will search for new recipes, using the selected ingredient.
-// HINT: Check out line 29!
+function showCountryData(data) {
+  // Add more code from Step 3
+}
 
-// PART 4: Display Multiple Recipes
+// Step 5: Dynamically search country using HTML form with eventHandler
 
-// Each time we request recipes, we get an array inside the `results` array.
-// How would you display all ten `results` at once?
-// Use the existing functions `findRecipes` and `showIngredients` to display all ten recipe results.
+// Write eventHandler here
 
-// PART 5 - Display Search bar results
+// Step 6: Remove previous country data
 
-// Try to display recipes using a search bar for text input.
-// HINT: Start by uncommenting the search bar in index.html
+function removeCountry() {
+ // write code here
+ // Make sure to call this function???
+}
